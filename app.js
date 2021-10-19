a50507173eb4a4854c8a02cc6f6cd398ee5c5a2e
codersait <codersait@gmail.com>
2021-10-19 16:24:56 +0300

    update

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
-// document object is created by the browser to model html page.
-// each elements is considered a node in DOM.html -> root node
-// text inside the element is text node 
-// * Query the DOM
-// document.querySelector() -- used any parent element not only document
-// document.querySelectorAll() -- used any parent element not only document
-// document.getElementById()
-// document.getElementsByClassName()
-// document.getElementsByTagName()
-// * Traverse the DOM
-// parent.children -- down to tree
-// child.parentElement -- up to tree
-// child.nextElementSibling
-// child.previousElementSibling
-// * Content
-// innerText -- visible text
-// textContent -- all text even spaces
-// innerHTML
-// * Append
-// += 
-// append
-// * Attributes
-// getAttribute('src')
-// setAttribute('src','source path')
-// * Styles
-// style property -- inline style
-// for removing style set it to '';
-// classList
-// classList.add()
-// classList.remove()
-// classList.toggle()
-// * Creating and Removing 
-// element.remove()
-// document.createElement('tag name')
-// parent.append()
-// parent.prepend()
-const h1 = document.querySelector('h1')
-console.log(h1.parentElement);
-console.log(h1.children);
+document object is created by the browser to model html page.
+each elements is considered a node in DOM.html -> root node
+text inside the element is text node 
+* Query the DOM
+document.querySelector() -- used any parent element not only document
+document.querySelectorAll() -- used any parent element not only document
+document.getElementById()
+document.getElementsByClassName()
+document.getElementsByTagName()
+* Traverse the DOM
+parent.children -- down to tree
+child.parentElement -- up to tree
+child.nextElementSibling
+child.previousElementSibling
+* Content
+innerText -- visible text
+textContent -- all text even spaces
+innerHTML
+* Append
++= 
+append
+* Attributes
+getAttribute('src')
+setAttribute('src','source path')
+* Styles
+style property -- inline style
+for removing style set it to '';
+classList
+classList.add()
+classList.remove()
+classList.toggle()
+* Creating and Removing 
+element.remove()
+document.createElement('tag name')
+parent.append()
+parent.prepend()
+
